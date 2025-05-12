f39e55655c30e44a09e8ddeb42991b275fd34c42
alexis-godbout <156146542+alexis-godbout@users.noreply.github.com>
2025-05-12 07:56:57 -0400

    v1

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# alias-generator
+# wordnet-data
+
+This repository contains a Node.js [script](./src/script.js) to fetch, expand, trim and store [Wordnet](https://wordnet.princeton.edu/) data files that can then be used by other projects.
+
+For reference, the Wordnet data structure is documented [here](https://wordnet.princeton.edu/documentation/wndb5wn).
+
+## Development
+
+1. Make sure that Node.js is installed
+2. Navigate to the [src](./src/) folder
+2. Install the dependencies
+
+```bash
+npm install
+```
+
+3. Make necessary adjustments
+
+4. Use eslint to review the code
+
+```bash
+npx eslint ./script.js
+```
---
+++ src/script.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+// Dependencies
+import { get } from 'node:https';
+import { extract } from 'tar';
+import { chmod, mkdir, open, readdir, writeFile } from 'node:fs/promises';
+
+// Variables
+const DOWNLOAD_URL = 'https://wordnetcode.princeton.edu/wn3.1.dict.tar.gz';
+const FILES_TO_EXTRACT = ['dict/data.adj', 'dict/data.adv', 'dict/data.noun', 'dict/data.verb'];
+const OUTPUT_DIRECTORY = '../data';
+
+// Functions
+function downloadAndExtractTarGz(url, fileList, outputDirectory) {
+    return new Promise((resolve, reject) => {
+        get(url, (response) => {
+            if (response.statusCode !== 200) {
+                reject(`Failed to download file. Status code: ${response.statusCode}`);
+            }
+            else {
+                const tarExtractor = extract(
+                    {
+                        cwd: outputDirectory,
+                        strip: 1
+                    },
+                    fileList
+                );
+                response.pipe(tarExtractor);
+                response.on('error', (errorMsg) => {
+                    reject(
+                        console.error(`Error during download: ${errorMsg}`)
+                    );
+                })
+                tarExtractor.on('error', (errorMsg) => {
+                    reject(
+                        console.error(`Error during extraction: ${errorMsg}`)
+                    );
+                })
+                tarExtractor.on('finish', () => {
+                    resolve(
+                        console.log(`Successfully downloaded and extracted ${url.split('/').pop()} to ${outputDirectory}`)
+                    );
+                })
+            }
+        });
+    })
+}
+
+async function trimWordnetDataFile(file) {
+    let newLines = [];
+    const regexLicense = /^\s{2}.*$/;
+    await chmod(file, '600');
+    const fileHandle = await open(file);
+    for await (const line of fileHandle.readLines()) {
+        if (regexLicense.test(line)) {
+            newLines.push(`${line}\n`);
+        }
+        else {
+            newLines.push(`${line.split(' ')[4]} | ${line.split('| ').pop().trim()}\n`);
+        }
+    }
+    await writeFile(file, newLines);
+}
+
+async function main() {
+    await mkdir(OUTPUT_DIRECTORY, { recursive: true });
+    await downloadAndExtractTarGz(DOWNLOAD_URL, FILES_TO_EXTRACT, OUTPUT_DIRECTORY);
+    for (const file of await readdir(OUTPUT_DIRECTORY)) {
+        trimWordnetDataFile(`${OUTPUT_DIRECTORY}/${file}`)
+    }
+}
+
+// Script
+
+main();
